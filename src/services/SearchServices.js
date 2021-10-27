efe5e6606e5ecf12628b00ea751da8e477d2a2ec
rinaldi.fauzi <rinaldi.fauzi@pgn-solution.co.id>
2021-10-27 11:22:15 +0700

    setting lazy load with jwt auth

--- src/services/SearchServices.js
+++ src/services/SearchServices.js
@@ -1,15 +1,6 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: `http://rfc.pgn-solution.co.id/api/v1/api_tests`,
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'X-Api-Key': 'Pg4550Lut1oN!'
-   }
-})
-
 export default function SearchServices(query, pageNumber){
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
@@ -26,14 +17,19 @@ export default function SearchServices(query, pageNumber){
         let cancel
         axios({
             method: 'GET',
-            url: 'http://openlibrary.org/search.json',
+            url: `http://localhost:1234/api/v1/api_tests`,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo3LCJleHAiOjE2MzUzMTE5NDB9.nHRZ8N-w8ZiJEaqene2qz_V_oaEJbpmZ8yS5k4UNErw'
+            },
             params:{q: query, page: pageNumber},
             cancelToken: new axios.CancelToken(c => cancel = c)
         }).then(res => {
             setBooks(prevBooks => {
-                return [...new Set([...prevBooks, ...res.data.docs.map(b => b.title)])]
+                return [...new Set([...prevBooks, ...res.data.body.map(b => b.nama)])]
             })
-            setHasMore(res.data.docs.length > 0)
+            setHasMore(res.data.body.length > 0)
             setLoading(false)
             console.log(res.data)
         }).catch(e => {
